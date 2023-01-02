34142118587e9d130db37f619aadcadfcd0290d1
Matteo Hertel <info@matteohertel.com>
2023-01-02 14:32:05 +0100

    Initial Commit

---
+++ LearniOS/LearniOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearniOSApp.swift
+//  LearniOS
+//
+//  Created by Matteo Hertel on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct LearniOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
